8f1907a45b5385347896e3ed4f4d8eadb4a2a294
RadukuX <41450735+RadukuX@users.noreply.github.com>
2020-01-10 23:02:19 +0200

    11 noaptea fratioare

--- app/src/main/java/com/example/debatetracker/ui/gallery/DebateLocation.java
+++ app/src/main/java/com/example/debatetracker/ui/gallery/DebateLocation.java
@@ -1,8 +1,10 @@
 package com.example.debatetracker.ui.gallery;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -50,6 +52,10 @@ public class DebateLocation extends FragmentActivity implements OnMapReadyCallba
     }
 
     private void fetchLastLocation() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+            return;
+        }
         Task<android.location.Location> task = fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<android.location.Location>() {
             @Override
